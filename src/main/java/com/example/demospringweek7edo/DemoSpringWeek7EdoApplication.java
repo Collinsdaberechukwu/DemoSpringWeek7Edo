e0b88a0416d734b9cccb787d7c76caf8bd64321d
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-10-03 22:26:16 +0100

    First update

---
+++ src/main/java/com/example/demospringweek7edo/DemoSpringWeek7EdoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringweek7edo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWeek7EdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWeek7EdoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringweek7edo/DemoSpringWeek7EdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringweek7edo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWeek7EdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
